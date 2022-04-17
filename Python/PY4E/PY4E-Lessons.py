cf876684714aaf9bdef357db3f36679fc0a7e5dd
Paul Anderson <pander414@gmail.com>
2022-04-17 14:13:12 -0700

    Update PY4E-Lessons.py

--- Python/PY4E/PY4E-Lessons.py
+++ Python/PY4E/PY4E-Lessons.py
@@ -1,7 +1,7 @@
 #I am going to use this file for the Py4E lesson
 found = False
 print("Before", found)
-for value in [9,41,12,74,15,34,3,85] :
+for value in [9,41,12,74,15,34,3] :
     if value == 3 :
         found = True
         break
